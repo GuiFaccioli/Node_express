7a86cacfb2e2777bb6293b493475d34b9f0931cc
Gui Faccioli <guifacciolic@gmail.com>
2026-03-29 08:24:10 -0300

    estudando as conexoes e como conectar

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 const express = require("express");
+const mysql = require("mysql2");
 const app = express();
-const port = 3000;
+app.use(express.json());
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "X1x2x3x4-",
+  database: "estudo",
+});
 
-const atendimentoRouter = require("./routers/atendimentoRoute");
+connection.connect((erro) => {
+  if (erro) {
+    console.log("Erro ao conectar no banco:", erro);
+    return;
+  }
+  console.log("Conectado ao MySQL com sucesso");
+});
 
-app.use(atendimentoRouter);
-
-app.listen(port, () => {
-  console.log(`O servidor está rodando na porta ${port}`);
+app.get("/usuarios", (req, res) => {
+  connection.query("SELECT * FROM usuario", (erro, resultados) => {
+    if (erro) {
+      return res.status(500).json({
+        erro: "Erro ao buscar usuários",
+      });
+    }
+    res.json(resultados);
+  });
 });
